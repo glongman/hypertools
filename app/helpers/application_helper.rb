b6294179d0871f0581dee7ae8d6a4c7bc15b8505
Geoff Longman <glongman@overlay.tv>
2009-08-19 22:45:06 -0400

    make this app less offensively ugly (suck less)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_top
+    if logged_in?
+      render :partial => 'home/logged_in_top'
+    elsif User.count == 0
+      render :partial => 'home/setup_top'
+    elsif controller.controller_name == 'sessions' && %w(new create).include?(controller.action_name)
+      render :partial => 'home/login_top'
+    else
+      render :partial => 'home/public_top'
+    end
+  end
+  
 end
